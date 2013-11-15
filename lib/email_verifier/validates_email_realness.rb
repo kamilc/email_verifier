6cb624dfa28f252211ee0ab68d6f636ff150df3d
Francesco Gnarra <francesco.gnarra@gmail.com>
2013-11-15 15:53:18 +0100

    Update validates_email_realness.rb

--- lib/email_verifier/validates_email_realness.rb
+++ lib/email_verifier/validates_email_realness.rb
@@ -7,13 +7,13 @@ module EmailVerifier
       class EmailRealnessValidator < ActiveModel::EachValidator
         def validate_each(record, attribute, value)
           begin
-            record.errors.add attribute, 'must point to a real mail account' unless EmailVerifier.check(value)
+            record.errors.add attribute, 'deve essere un indirizzo reale' unless EmailVerifier.check(value)
           rescue EmailVerifier::OutOfMailServersException
-            record.errors.add attribute, 'appears to point to dead mail server'
+            record.errors.add attribute, 'punta ad un server mail inesistente'
           rescue EmailVerifier::NoMailServerException
-            record.errors.add attribute, "appears to point to domain which doesn't handle e-mail"
+            record.errors.add attribute, "punta ad un dominio che non gestisce le email"
           rescue EmailVerifier::FailureException
-            record.errors.add attribute, "could not be checked if is real"
+            record.errors.add attribute, "deve essere un indirizzo reale"
           end
         end
       end
