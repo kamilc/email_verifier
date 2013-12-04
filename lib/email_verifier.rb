dbd3fba3642b9daa08a8cc5f2e8edb91f7de8e7f
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2013-12-04 13:08:55 +0100

    Provided autoloading default localization for English and added a localization for Polish locales

--- lib/email_verifier.rb
+++ lib/email_verifier.rb
@@ -6,6 +6,15 @@ module EmailVerifier
   require 'email_verifier/config'
   require "email_verifier/validates_email_realness"
 
+  if defined?(Rails::Railtie)
+    class Railtie < ::Rails::Railtie #:nodoc:
+      initializer 'rails-i18n' do |app|
+        I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locales'), '*.yml')]
+        I18n.load_path.flatten!
+      end
+    end
+  end
+
   def self.check(email)
     v = EmailVerifier::Checker.new(email)
     v.connect
@@ -20,3 +29,4 @@ module EmailVerifier
     end
   end
 end
+
